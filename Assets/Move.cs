629354f14f29ec3fe076d35fa03ad128986375d2
christinaalborova111 <90421814+christinaalborova111@users.noreply.github.com>
2022-01-24 00:04:01 +0300

    Created jump system

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -6,10 +6,20 @@ public class Move : MonoBehaviour
 {   
     [SerializeField]
     private float speed=1f;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
         transform.position += transform.forward * Input.GetAxis("Vertical")*Time.deltaTime*speed;
         transform.position += transform.right * Input.GetAxis("Horizontal")*Time.deltaTime*speed;
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            rb.AddForce(transform.up*speed,ForceMode.Impulse);
+        }
     }
 }
